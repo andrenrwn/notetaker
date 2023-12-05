2c8953d5c2e1b8ec6f862497277cd833ef9dfc9b
anrwn <andrenrwn@users.noreply.github.com>
2023-12-06 02:36:49 +0800

    grading criteria. Fixed __dirname for ES6 for express static files. See https://github.com/nodejs/help/issues/2907

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ import express from 'express';
 
 import { router as routes } from './routes/index.mjs';
 
-// import path from 'path';
-// import express from 'express';
-
-// import { router as routes } from './routes/index.mjs';
-
 // Fix for ReferenceError: __dirname is not defined in ES module scope
 // https://github.com/nodejs/help/issues/2907
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+// /fix
+
+// Fix for ReferenceError: __dirname is not defined in ES module scope
+// https://github.com/nodejs/help/issues/2907
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 // /fix
 
 const app = express();
